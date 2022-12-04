48f72a8128fdc7837b664bc247f2705b2ae7d353
Ethan Shafer <ehs70@case.edu>
2022-12-04 01:02:38 -0500

    Day 4 part 1

---
+++ day4.py
@@ -0,0 +1,31 @@
+from utilities import read_file
+from typing import List
+
+def split_numbers(input_lines):
+    for index in range(len(input_lines)):
+        split = input_lines[index].split(',')
+        string_list = split[0].split('-')
+        string_list.extend(split[1].split('-'))
+        input_lines[index] = [int(x) for x in string_list]
+
+def unwind_numbers(splits):
+    print(splits)
+    for index in range(len(splits)):
+        splits[index] = set(range(splits[index][0], splits[index][1]+1)), set(range(splits[index][2], splits[index][3]+1))
+
+def ranges_overlap(ranges: List[set]):
+    for range in ranges:
+        yield range[0].issubset(range[1]) or range[1].issubset(range[0])
+
+def part_one(sample_file = True):
+    if not sample_file:
+        in_lines = read_file('data/day4_input')
+    else:
+        in_lines = read_file('data/day4_sample')
+
+    split_numbers(in_lines)
+    unwind_numbers(in_lines)
+
+    return sum(1 for x in ranges_overlap(in_lines) if x)
+
+print(part_one(False))
